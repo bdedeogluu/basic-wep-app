f3854465c8bca7845382955143bd8cd4bf3f8317
bdedeogluu <62393670+bdedeogluu@users.noreply.github.com>
2021-03-09 21:12:07 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import './App.scss';
+
+const App = () => {
+
+  const [location, setLocation] = useState('Alanya');
+  const [weatherInfo, setWeatherInfo] = useState({})
+  const [city, setCity] = useState('')
+
+  const params = {
+    key: 'b928a896daee4e71b26225453210603',
+    location
+  }
+
+  useEffect(() => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${params.key}&q=${params.location}`)
+      .then(response => response.json())
+      .then(data => setWeatherInfo(data))
+  }, [location])
+
+
+  return (
+    <div className="weather-wrapper">
+      <input
+        onKeyDown={(event) => event.keyCode === 13 ? setLocation(city) : null}
+        onChange={(event) => setCity(event.target.value)}
+        onBlur={() => setLocation(city)}
+      />
+      <div className="weather-card">
+        {weatherInfo.current && (
+          <>
+            <img className="weather-icon" src={weatherInfo.current.condition.icon} alt={'asdsadas'} />
+            <h1>{weatherInfo.current.temp_c}</h1>
+            <p>{weatherInfo.location.name} - <span>{weatherInfo.current.condition.text}</span></p>
+          </>
+        )
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
